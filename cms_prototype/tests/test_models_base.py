11a32c5ad6c90cdb9f730bbef4c1a0de102790f2
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-18 21:55:51 +0000

    added start of revisioned document tests

--- cms_prototype/tests/test_models_base.py
+++ cms_prototype/tests/test_models_base.py
@@ -1,13 +1,18 @@
 import unittest
-from cms_prototype.models.base import VersionedDocument
+from pymongo import MongoClient
 
 class VersionedDocument(unittest.TestCase):
 	def setUp(self):
 		connection = MongoClient('localhost', 27017)
 		self.db = connection.local
-		self.db.versioneddocument.remove()
+		self.db.versioned_document.remove()
+		self.db.versioned_versioned_document.remove()
 
 	def test_single_revision(self):
+		from cms_prototype.models.base import VersionedDocument
 		v = VersionedDocument()
-		q = self.db.versioneddocument.find({'_id': v._id})
-		self.assertEqual(q.length, 1)
+		v.save()
+		q = self.db.versioned_document.find()
+		self.assertEqual(q.count(), 1)
+		q = self.db.versioned_document.find({'_id': v._id})
+		self.assertEqual(q.count(), 1)
