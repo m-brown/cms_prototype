5a75e6ea0af6ab907a9dc5e2a10bfd5175d13e5a
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-21 21:44:05 +0000

    simplified and extended tests

--- cms_prototype/tests/test_models_base.py
+++ cms_prototype/tests/test_models_base.py
@@ -9,26 +9,51 @@ class SomeTestDocument(VersionedDocument):
     some_key = StringField(max_length=50)
 
 class VersionedDocumentTestCase(TestCase):
-
     def setUp(self):
         super(VersionedDocumentTestCase, self).setUp()
         self.db = SomeTestDocument._get_collection().database
         self.db.some_test_document.remove()
         self.db.versioned_some_test_document.remove()
 
+    def test_base_fields(self):
+        test_doc = SomeTestDocument(some_key='test')
+
+        self.assertEquals(test_doc.id, None)
+
+        test_doc.save()
+
+        self.assertNotEquals(test_doc.id, None)
+        self.assertNotEquals(test_doc._rev, None)
+        self.assertNotEquals(test_doc._ts, None)
+        self.assertEquals(test_doc._parent, None)
+
+    def test_parent_history(self):
+        test_doc = SomeTestDocument(some_key='test')
+        test_doc.save()
+        previous_rev = test_doc._rev
+        test_doc.save()
+        self.assertNotEquals(test_doc._rev, previous_rev)
+        self.assertEquals(test_doc._parent, previous_rev)
+
     def test_single_revision(self):
         test_doc = SomeTestDocument(some_key='test value')
         test_doc.save()
 
-        vid = test_doc.id
-        q = self.db.some_test_document.find()
-        self.assertEqual(q.count(), 1)
-        q = self.db.some_test_document.find({'_id': vid})
-        self.assertEqual(q.count(), 1)
-        q = self.db.versioned_some_test_document.find()
-        self.assertEqual(q.count(), 1)
-        q = self.db.versioned_some_test_document.find({'_id.id': vid})
-        self.assertEqual(q.count(), 1)
+        self.assertEqual(self.db.some_test_document.find().count(), 1)
+        self.assertEqual(self.db.some_test_document.find({'_id': test_doc.id}).count(), 1)
+        self.assertEqual(self.db.versioned_some_test_document.find().count(), 1)
+        self.assertEqual(self.db.versioned_some_test_document.find({'_id.id': test_doc.id}).count(), 1)
+
+    def test_double_revision(self):
+    	test_doc = SomeTestDocument(some_key='test value')
+        test_doc.save()
+        test_doc.some_key = 'some other value'
+        test_doc.save()
+
+        self.assertEqual(self.db.some_test_document.find().count(), 1)
+        self.assertEqual(self.db.some_test_document.find({'_id': test_doc.id}).count(), 1)
+        self.assertEqual(self.db.versioned_some_test_document.find().count(), 2)
+        self.assertEqual(self.db.versioned_some_test_document.find({'_id.id': test_doc.id}).count(), 2)
 
     def test_compound_key_revision(self):
     	from cms_prototype.models.site import Site, Url, UrlKey
