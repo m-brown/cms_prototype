988e0f1bd39fc320ec7d917a0d3e3600c866d247
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-24 19:33:17 +0000

    add basic site structure models

--- cms_prototype/models/site.py
+++ cms_prototype/models/site.py
@@ -1,15 +1,20 @@
-from mongoengine import EmbeddedDocumentField, ReferenceField, StringField
+from mongoengine import EmbeddedDocumentField, ReferenceField, StringField, DictField, ReferenceField
 from mongoengine import Document, EmbeddedDocument
 
 from cms_prototype.models.base import VersionedDocument
 
 class Site(VersionedDocument):
-	name 		= StringField()
-	unique_name = StringField()
+    name        = StringField()
+    unique_name = StringField()
+
+class Page(VersionedDocument):
+    handler     = StringField()
+    parameters  = DictField()
 
 class UrlKey(EmbeddedDocument):
-    url  = StringField()
-    site = ReferenceField('Site', dbref=False)
+    url         = StringField()
+    site        = ReferenceField('Site', dbref=False)
 
 class Url(Document):
-    key = EmbeddedDocumentField('UrlKey', primary_key=True)
+    key         = EmbeddedDocumentField('UrlKey', primary_key=True)
+    page        = ReferenceField(Page, reverse_delete_rule=DENY)
