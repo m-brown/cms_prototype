d07794d3b3fbdf4768f91bdc002e93a764392dba
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-25 22:32:54 +0000

    updated main to be called without config

--- cms_prototype/__init__.py
+++ cms_prototype/__init__.py
@@ -1,11 +1,14 @@
 from pyramid.config import Configurator
 
 
-def main(global_config, **settings):
+def main(global_config='', **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    if settings:
+        config = Configurator(settings=settings)
+    else:
+        config = Configurator()
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('page', '/{site_unquie_name}/{url}')
     config.scan()
     return config.make_wsgi_app()
