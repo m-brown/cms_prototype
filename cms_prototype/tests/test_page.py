15d1c3092d325496a3e82f54bfc60f4bd2cbfc07
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-25 20:48:55 +0000

    basic test cases for pages

--- cms_prototype/tests/test_page.py
+++ cms_prototype/tests/test_page.py
@@ -4,19 +4,27 @@ from cms_prototype.tests.common import TestCase
 from cms_prototype.models.site import Site, Page, UrlKey, Url
 
 class PageloadTest(TestCase):
-	def setUp(self):
-		site = Site(name='test', unique_name='test')
-		site.save()
-		page = Page()
-		page.save()
-		urlKey = UrlKey(site=site, url='index.html')
-		url = Url(key=urlKey, page=page)
-		url.save()
+    def setUp(self):
+        site = Site(name='test', unique_name='test')
+        site.save()
+        page = Page()
+        page.save()
+        urlKey = UrlKey(site=site, url='index.html')
+        url = Url(key=urlKey, page=page)
+        url.save()
 
-		from cms_prototype import main
-		app = main()
-		from webtest import TestApp
-		self.testapp = TestApp(app)
+        from cms_prototype import main
+        app = main()
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_missing_project(self):
+        res = self.testapp.get('/somerandomprojectthatdoesnotexist/', status=404)
+        self.assertTrue('Not Found' in res.body)
+
+    def test_missing_page(self):
+        res = self.testapp.get('/test/somerandompagethatdoesnotexist.html', status=404)
+        self.assertTrue('Not Found' in res.body)
 
 	def test_page_load(self):
-		response = self.testapp.get('/test/index.html')
+		res = self.testapp.get('/test/index.html', status=200)
