661a36efc5f8ddc4e5c86e4e10c07b70e1af27f8
Matthew Brown <matthew.brown@ukplc.net>
2013-02-26 17:15:06 +0000

    added skeleton url lookup

--- cms_prototype/views/page.py
+++ cms_prototype/views/page.py
@@ -1,3 +1,7 @@
+from cms_prototype.models.site import Site, Url, UrlKey
 
 def page(request):
-    return {}
+    site = Site.objects(unique_name=request.unique_name)
+    urlKey = UrlKey(site=site, url=request.url)
+    url = Url.objects(key=urlKey)
+    return url
