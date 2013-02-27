62733bfe3b791d53d40ba0a5b5719b11601dbf67
Matthew Brown <dr.m.w.brown@gmail.com>
2013-02-27 19:43:01 +0000

    added bock and updated site

--- cms_prototype/models/site.py
+++ cms_prototype/models/site.py
@@ -3,13 +3,19 @@ from mongoengine import Document, EmbeddedDocument
 
 from cms_prototype.models.base import VersionedDocument
 
-class Site(VersionedDocument):
+class Block(VersionedDocument):
     name        = StringField()
-    unique_name = StringField()
 
 class Page(VersionedDocument):
     handler     = StringField()
     parameters  = DictField()
+    body        = StringField()
+
+class Site(VersionedDocument):
+    name        = StringField()
+    unique_name = StringField()
+    header      = ReferenceField(Block, dbref=True)
+    footer      = ReferenceField(Block, dbref=True)
 
 class UrlKey(EmbeddedDocument):
     site        = ReferenceField('Site', dbref=False)
