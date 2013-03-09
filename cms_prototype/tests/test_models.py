95a8906c00c0e64307557874a8f35f94a6627791
Matthew Brown <dr.m.w.brown@gmail.com>
2013-03-09 13:20:43 +0000

    fixed layout tests

--- cms_prototype/tests/test_models.py
+++ cms_prototype/tests/test_models.py
@@ -7,6 +7,7 @@ from cms_prototype.models.site import Layout, Block
 class LayoutDoc(VersionedDocument):
     layout = EmbeddedDocumentField(Layout)
 
+
 class LayoutTestCase(TestCase):
 
     def setUp(self):
@@ -48,56 +49,61 @@ class LayoutTestCase(TestCase):
 
     def test_add_layout(self):
         ld = LayoutDoc()
-        ld.layout.layout.append(Layout())
+        ld.layout = Layout()
+        ld.layout.items.append(Layout())
         ld.save()
 
-        self.assertEqual(len(ld._layouts), 1)
+        self.assertEqual(len(ld.layout.items), 1)
         self.assertEqual(self.db.layout_doc.find().count(), 1)
         self.assertEqual(self.db.layout.find().count(), 0)
 
     def test_add_child_block(self):
         ld = LayoutDoc()
-        l = LayoutDoc()
+        ld.layout = Layout()
+        l = Layout()
         b = Block(name='test')
         b.save()
-        l.add(b)
+        l.items.append(b)
+        ld.layout.items.append(l)
 
-        ld.add(l)
         ld.save()
 
-        self.assertEqual(len(ld._layouts), 1)
+        self.assertEqual(len(ld.layout.items), 1)
         self.assertEqual(self.db.layout_doc.find().count(), 1)
         self.assertEqual(self.db.layout.find().count(), 0)
 
     def test_remove_block(self):
         ld = LayoutDoc()
+        ld.layout = Layout()
         b = Block(name='test')
         b.save()
 
-        ld.add(b)
+        ld.layout.items.append(b)
         ld.save()
 
-        self.assertEqual(len(ld._blocks), 1)
+        self.assertEqual(len(ld.layout.items), 1)
 
-        ld.remove(b)
+        ld.layout.items.remove(b)
         ld.save()
-        self.assertEqual(len(ld._blocks), 0)
+        self.assertEqual(len(ld.layout.items), 0)
 
     def test_remove_illegal(self):
         ld = LayoutDoc()
+        ld.layout = Layout()
         b = Block(name='test')
         b.save()
 
-        ld.add(b)
+        ld.layout.items.append(b)
         ld.save()
         with self.assertRaises(Exception):
-            ld.remove('test')
+            ld.layout.items.remove('test')
 
     def test_remove_not_present(self):
         ld = LayoutDoc()
+        ld.layout = Layout()
         b = Block(name='test')
         b.save()
 
         ld.save()
         with self.assertRaises(Exception):
-            ld.remove(b)
+            ld.layout.items.remove(b)
