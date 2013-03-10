62a8e43ce6d34dca73b28691acea14744b86dd36
Matthew Brown <dr.m.w.brown@gmail.com>
2013-03-10 21:41:22 +0000

    lookup block

--- cms_prototype/views/block.py
+++ cms_prototype/views/block.py
@@ -1,5 +1,6 @@
 from cms_prototype.models.site import Block
 
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
 from pyramid.view import view_config
 
@@ -7,5 +8,9 @@ from pyramid.view import view_config
 @view_config(route_name='block')
 def block(request):
     db = Block._get_collection().database
-    base = db.block.find(query={id: request.matchdict['block']}).first()
-    return Response()
+    b = Block.objects(id=request.matchdict['block']).first()
+
+    if not b:
+        raise HTTPNotFound()
+    else:
+        return Response(b)
