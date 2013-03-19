596c3f6c5a290ebb402434bc6e95a3ee339882bd
Matthew Brown <dr.m.w.brown@gmail.com>
2013-03-19 22:15:51 +0000

    added block path to routing

--- cms_prototype/__init__.py
+++ cms_prototype/__init__.py
@@ -12,6 +12,7 @@ def main(global_config='', **settings):
 
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('page', '/{site_unique_name}/{url}')
+    config.add_route('block', '/_block/{block}')
 
     config.scan('cms_prototype.views.page')
 
