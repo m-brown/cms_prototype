34bedf2bd55864663e57295c96fbd98d8e7a151b
Matthew Brown <dr.m.w.brown@gmail.com>
2013-03-28 21:35:22 +0000

    removed old code around layouts

--- cms_prototype/models/site.py
+++ cms_prototype/models/site.py
@@ -33,33 +33,3 @@ class Layout(EmbeddedDocument):
     html_class  = StringField()
     items       = ListField(SwitchableTypeField((EmbeddedDocumentField('Layout'),
                                                  ReferenceField(Block, dbref=False))))
-
-    def add(self, obj):
-        nextPos = len(_layouts) + len(_blocks)
-        if issubclass(type(obj), Layout):
-            _layouts[nextPos] = obj
-        elif issubclass(type(obj), Block):
-            _blocks[nextPos] = obj
-        else:
-            raise Exception('Can only add Blocks or other Layouts to a Layout')
-
-    def remove(self, obj):
-        if issubclass(type(obj), Layout):
-            for layout in _layouts:
-                if _layouts[layout] == obj:
-                    del _layouts[layout]
-        elif issubclass(type(obj), Block):
-            for block in _blocks:
-                if _blocks[block] == obj:
-                    del _blocks[block]
-        else:
-            raise Exception('Can only remove Blocks or Layouts')
-
-    def save(self, *args, **kwargs):
-        Document.save.__doc__
-
-        for i in range(len(_layouts)+len(_blocks)):
-            if i not in _layouts or i not in _blocks:
-                raise Exception('Missing possition in layout')
-
-        return super(Layout, self).save(*args, **kwargs)
