f28fa1fb5bc44660382a79649a3f6f15255c4f3e
Matthew Brown <drmwbrown@gmail.com>
2013-04-09 19:58:23 +0100

    simple test for handler

--- cms_prototype/tests/test_page_handler.py
+++ cms_prototype/tests/test_page_handler.py
@@ -1,10 +1,19 @@
 from cms_prototype.tests.common import TestCase
 from cms_prototype.models.page_handler import PageHandler
 from cms_prototype.models.site import Page
+from cms_prototype.models.layout import Layout
+from cms_prototype.models.blocks.text import Text
 from nose.plugins.skip import SkipTest
 
 
 class Handler(TestCase):
 
-    def test_render(self):
-        raise SkipTest
+    def test_non-modified_render(self):
+        l = Layout()
+        l.items.append(Text(text='foo'))
+        p = Page(layout=l)
+
+        h = PageHandler(page=p)
+        html = h.render()
+
+        self.assertEquals(html, '<div class="layout"><div class="block">foo</div></div>')
