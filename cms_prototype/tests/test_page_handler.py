c6f92f31e0a18c1d0f34f19523bad38a680a5f46
Matthew Brown <matthew.brown@ukplc.net>
2013-04-11 13:29:55 +0100

    fixed wrong class + output formatting

--- cms_prototype/tests/test_page_handler.py
+++ cms_prototype/tests/test_page_handler.py
@@ -2,17 +2,17 @@ from cms_prototype.tests.common import TestCase
 from cms_prototype.models.page_handler import PageHandler
 from cms_prototype.models.site import Page
 from cms_prototype.models.layout import Layout
-from cms_prototype.models.blocks.text import Text
+from cms_prototype.models.blocks.text import HTMLBlock
 
 
 class Handler(TestCase):
 
     def test_non_modified_render(self):
         l = Layout()
-        l.items.append(Text(text='foo'))
+        l.items.append(HTMLBlock(text='foo'))
         p = Page(layout=l)
 
         h = PageHandler(page=p)
         html = h.render()
 
-        self.assertEquals(html, '<div class="layout"><div class="block">foo</div></div>')
+        self.assertEquals(html, '<div class="layout">\n<div class="block">\nfoo\n</div>\n</div>')
