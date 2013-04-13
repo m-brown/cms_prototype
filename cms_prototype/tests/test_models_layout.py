7aea90906236dc45396de04308ddd2058168985a
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-13 13:09:03 +0100

    fixed most of the rendering issues

--- cms_prototype/tests/test_models_layout.py
+++ cms_prototype/tests/test_models_layout.py
@@ -1,4 +1,4 @@
-from cms_prototype.tests.common import TemplateTestCase
+from cms_prototype.tests.common import TemplateTestCase, strip_html_whitespace
 from cms_prototype.models.blocks.text import HTMLBlock
 from cms_prototype.models.layout import Layout
 
@@ -7,15 +7,15 @@ class LayoutTest(TemplateTestCase):
 
     def test_id(self):
         l = Layout(html_id="foo")
-        html = l.render()
+        html = strip_html_whitespace(l.render())
 
-        self.assertEqual(html, '<div id="foo" class="layout">\n</div>')
+        self.assertEqual(html, '<div id="foo" class="layout"></div>')
 
     def test_class(self):
         l = Layout(html_class="foo")
-        html = l.render()
+        html = strip_html_whitespace(l.render())
 
-        self.assertEqual(html, '<div class="layout foo">\n</div>')
+        self.assertEqual(html, '<div class="layout foo"></div>')
 
     def test_single_depth_single_block(self):
         b = HTMLBlock(text='foo')
@@ -23,9 +23,9 @@ class LayoutTest(TemplateTestCase):
         l = Layout()
         l.items.append(b)
 
-        html = l.render()
+        html = strip_html_whitespace(l.render())
 
-        self.assertEqual(html, '<div class="layout">\n<div class="block">\nfoo\n</div>\n</div>')
+        self.assertEqual(html, '<div class="layout"><div class="block">foo</div></div>')
 
     def test_single_depth_multi_block(self):
         b = HTMLBlock(text='foo')
@@ -36,9 +36,9 @@ class LayoutTest(TemplateTestCase):
         l.items.append(b)
         l.items.append(b2)
 
-        html = l.render()
+        html = strip_html_whitespace(l.render())
 
-        self.assertEqual(html, '<div class="layout">\n<div class="block">\nfoo\n</div>\n<div class="block">\nbar\n</div>\n</div>')
+        self.assertEqual(html, '<div class="layout"><div class="block">foo</div><div class="block">bar</div></div>')
 
     def test_double_depth_single_blocks(self):
         b = HTMLBlock(text='foo')
@@ -52,6 +52,6 @@ class LayoutTest(TemplateTestCase):
         l2.items.append(b2)
         l.items.append(l2)
 
-        html = l.render()
+        html = strip_html_whitespace(l.render())
 
-        self.assertEqual(html, '<div class="layout">\n<div class="block">\nfoo\n</div>\n<div class="layout">\n<div class="block">\nbar\n</div>\n</div>\n</div>')
+        self.assertEqual(html, '<div class="layout"><div class="block">foo</div><div class="layout"><div class="block">bar</div></div></div>')
