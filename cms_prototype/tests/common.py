75b4fe8f57a757e80cc2c083eed5d7e2462ec8cd
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-13 12:28:27 +0100

    helper function to strip whitespace in html to make tests easier

--- cms_prototype/tests/common.py
+++ cms_prototype/tests/common.py
@@ -2,6 +2,7 @@ import os
 from pyramid import testing
 from mongoengine import connect
 from unittest import TestCase as _TestCase
+import re
 
 
 class TestCase(_TestCase):
@@ -25,3 +26,32 @@ class TemplateTestCase(TestCase):
         if mako_mod_directory:
             self.config.add_settings({'mako.module_directory': mako_mod_directory})
         self.config.include('pyjade.ext.pyramid')
+
+
+class TestTemplateTestCase(_TestCase):
+    def test_whitespace(self):
+        t = '<div id="foo">         \n</div>'
+        c = '<div id="foo"></div>'
+        r = strip_html_whitespace(t)
+
+        self.assertEquals(c, r)
+
+    def test_leading_whitespace(self):
+        t = '\n <div id="foo">'
+        c = '<div id="foo">'
+        r = strip_html_whitespace(t)
+
+        self.assertEquals(c, r)
+
+    def test_trailing_whitespace(self):
+        t = '</div> \n'
+        c = '</div>'
+        r = strip_html_whitespace(t)
+
+        self.assertEquals(c, r)
+
+
+def strip_html_whitespace(html):
+    html = re.sub(r'>\s+<', '><', html)
+    html = re.sub(r'^\s+<', '<', html)
+    return re.sub(r'>\s+$', '>', html)
