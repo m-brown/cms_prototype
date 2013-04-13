d861d29272552aec13c4dcefa6f667752d0117ce
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-13 17:02:00 +0100

    refactored handler + tests

--- cms_prototype/models/page_handler.py
+++ cms_prototype/models/page_handler.py
@@ -1,5 +1,12 @@
 """ Base handler class which controls the code side of each page load.
     Custom pages should inherit from this and override preload and postload.
+
+    There are 5 steps once we have identified which page this is:
+    1. Load initial data (__init__)
+    2. Allow any code to exectute before blocks are processed (pre_block_process)
+    3. Process blocks
+    4. Allow any code to execture after blocks are processed (post_block_process)
+    5. Render
 """
 class PageHandler:
     def __init__(self, page):
@@ -8,14 +15,14 @@ class PageHandler:
     """
         Code to run before blocks are processed
     """
-    def preload(self):
+    def pre_block_process(self):
         return
 
     """
         Code to be run after blocks are processed
     """
-    def postload(self):
+    def post_block_process(self):
         return
 
     def render(self):
-        return
+        return self.page.layout.render()
--- cms_prototype/tests/test_page_handler.py
+++ cms_prototype/tests/test_page_handler.py
@@ -1,18 +1,49 @@
-from cms_prototype.tests.common import TestCase
+from cms_prototype.tests.common import TemplateTestCase, strip_html_whitespace
 from cms_prototype.models.page_handler import PageHandler
 from cms_prototype.models.site import Page
 from cms_prototype.models.layout import Layout
 from cms_prototype.models.blocks.text import HTMLBlock
 
 
-class Handler(TestCase):
+class PreProcessHandler(PageHandler):
+    def pre_block_process(self):
+        b = HTMLBlock(text='bar')
+        b.save()
+        self.layout.items.append(b)
+
+
+class PostProcessHandler(PageHandler):
+    def post_block_process(self):
+        self.layout.items[0].text = 'bar'
+
+
+class Handler(TemplateTestCase):
+
+    def setUp(self):
+        super(TemplateTestCase, self).setUp()
+        b = HTMLBlock(text='foo')
+        b.save()
+        l = Layout()
+        l.items.append(b)
+        self.p = Page(layout=l)
+        self.p.save()
 
     def test_non_modified_render(self):
-        l = Layout()
-        l.items.append(HTMLBlock(text='foo'))
-        p = Page(layout=l)
+        h = PageHandler(page=self.p)
+        html = strip_html_whitespace(h.render())
 
-        h = PageHandler(page=p)
-        html = h.render()
+        self.assertEquals(html, '<div class="layout"><div class="block">foo</div></div>')
 
-        self.assertEquals(html, '<div class="layout">\n<div class="block">\nfoo\n</div>\n</div>')
+    def test_pre_process(self):
+        TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PreProcessHandler'], -1).PreProcessHandler
+        h = TestHandler(page=self.p)
+        html = strip_html_whitespace(h.render())
+
+        self.assertEquals(html, '<div class="layout"><div class="block">foo</div><div class="block">bar</div></div>')
+
+    def test_post_process(self):
+        TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PostProcessHandler'], -1).PostProcessHandler
+        h = TestHandler(page=self.p)
+        html = strip_html_whitespace(h.render())
+
+        self.assertEquals(html, '<div class="layout"><div class="block">bar</div></div>')
