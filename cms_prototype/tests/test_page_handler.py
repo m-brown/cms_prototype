11e4e774331ae69d51fb8048dcefeec2a3dae986
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-22 19:51:16 +0100

    fixed page handler tests

--- cms_prototype/tests/test_page_handler.py
+++ cms_prototype/tests/test_page_handler.py
@@ -9,12 +9,12 @@ class PreProcessHandler(PageHandler):
     def pre_block_process(self):
         b = HTMLBlock(text='bar')
         b.save()
-        self.layout.items.append(b)
+        self.page.layout.items.append(b)
 
 
 class PostProcessHandler(PageHandler):
     def post_block_process(self):
-        self.layout.items[0].text = 'bar'
+        self.page.layout.items[0].text = 'bar'
 
 
 class HandlerTestCase(TemplateTestCase):
@@ -30,6 +30,9 @@ class HandlerTestCase(TemplateTestCase):
 
     def test_non_modified_render(self):
         h = PageHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
         self.assertEquals(html, '<div class="layout"><div class="block">foo</div></div>')
@@ -37,6 +40,9 @@ class HandlerTestCase(TemplateTestCase):
     def test_pre_process(self):
         TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PreProcessHandler'], -1).PreProcessHandler
         h = TestHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
         self.assertEquals(html, '<div class="layout"><div class="block">foo</div><div class="block">bar</div></div>')
@@ -44,6 +50,9 @@ class HandlerTestCase(TemplateTestCase):
     def test_post_process(self):
         TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PostProcessHandler'], -1).PostProcessHandler
         h = TestHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
         self.assertEquals(html, '<div class="layout"><div class="block">bar</div></div>')
