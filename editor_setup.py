269fac51d49288c3c5a47ad5e97c77e91ca84027
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-28 15:30:50 +0100

    create site page

--- editor_setup.py
+++ editor_setup.py
@@ -5,6 +5,7 @@ from cms_prototype.models.site import Site, Page, Url, UrlKey
 from cms_prototype.models.site import Layout
 from cms_prototype.models.blocks.text import HTMLBlock
 from cms_prototype.models.blocks.link import Link
+from cms_prototype.models.blocks.form import Form, Input
 
 
 connect('cms', host=os.getenv('DB_HOST', 'localhost'))
@@ -21,7 +22,7 @@ s.save()
 l = Layout()
 heading = HTMLBlock(text='<h1>Site not found!</h1>')
 heading.save()
-link = Link(href='site', text='Create?', html_class='btn btn-primary')
+link = Link(href='create', text='Create?', html_class='btn btn-primary')
 link.save()
 l.items.append(heading)
 l.items.append(link)
@@ -29,5 +30,21 @@ p = Page(name='Site not found', layout=l)
 p.save()
 
 k = UrlKey(site=s, url='notfound')
+url = Url(key=k, page=p)
+url.save()
+
+
+#create new site
+f = Form()
+f.fields.append(Input(name='name', label='Name'))
+f.fields.append(Input(name='unique_name', label='Unique Name'))
+f.fields.append(Input(name='submit', type='submit'))
+f.save()
+l = Layout()
+l.items.append(f)
+p = Page(name='Create Site', layout=l)
+p.save()
+
+k = UrlKey(site=s, url='create')
 url = Url(key=k, page=p)
 url.save()
