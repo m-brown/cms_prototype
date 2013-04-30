b805792ce44edc1e7e3c9cac71caa0683fa3e587
Matthew Brown <dr.m.w.brown@gmail.com>
2013-04-30 22:05:36 +0100

    added static content to page handler tests

--- cms_prototype/tests/test_page_handler.py
+++ cms_prototype/tests/test_page_handler.py
@@ -5,16 +5,52 @@ from cms_prototype.models.layout import Layout
 from cms_prototype.models.blocks.text import HTMLBlock
 
 
+WRAPPER = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="/static/bootstrap/css/bootstrap.css">
+        <script src="/static/js/jquery.js"></script>
+        <script src="/static/bootstrap/js/bootstrap.js"></script>
+    </head>
+    <body>
+"""
+NON_MOD_HTML = WRAPPER + """
+        <div class="layout">
+            <div class="block">foo</div>
+        </div>
+    </body>
+</html>
+"""
+
+PRE_MOD_HTML = WRAPPER + """
+        <div class="layout">
+            <div class="block">foo</div>
+            <div class="block">bar</div>
+        </div>
+    </body>
+</html>
+"""
+
+POST_MOD_HTML = WRAPPER + """
+        <div class="layout">
+            <div class="block">bar</div>
+        </div>
+    </body>
+</html>
+"""
+
+
 class PreProcessHandler(PageHandler):
     def pre_block_process(self):
         b = HTMLBlock(text='bar')
         b.save()
-        self.layout.items.append(b)
+        self.page.layout.items.append(b)
 
 
 class PostProcessHandler(PageHandler):
     def post_block_process(self):
-        self.layout.items[0].text = 'bar'
+        self.page.layout.items[0].text = 'bar'
 
 
 class Handler(TemplateTestCase):
@@ -30,20 +66,29 @@ class Handler(TemplateTestCase):
 
     def test_non_modified_render(self):
         h = PageHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
-        self.assertEquals(html, '<div class="layout"><div class="block">foo</div></div>')
+        self.assertEquals(html, strip_html_whitespace(NON_MOD_HTML))
 
     def test_pre_process(self):
         TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PreProcessHandler'], -1).PreProcessHandler
         h = TestHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
-        self.assertEquals(html, '<div class="layout"><div class="block">foo</div><div class="block">bar</div></div>')
+        self.assertEquals(html, strip_html_whitespace(PRE_MOD_HTML))
 
     def test_post_process(self):
         TestHandler = __import__('cms_prototype.tests.test_page_handler', globals(), locals(), ['PostProcessHandler'], -1).PostProcessHandler
         h = TestHandler(page=self.p)
+        h.pre_block_process()
+        h.block_process()
+        h.post_block_process()
         html = strip_html_whitespace(h.render())
 
-        self.assertEquals(html, '<div class="layout"><div class="block">bar</div></div>')
+        self.assertEquals(html, strip_html_whitespace(POST_MOD_HTML))
